bd30bf5879d14bf64147c991616f061e0298df35
ThomasShetler <tshetler93@gmail.com>
2017-07-24 15:11:42 -0400

    Initial setup

---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>PostsIndex</div>
+    );
+  }
+}
+
+export default PostsIndex;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
